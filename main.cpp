5d3097eaf9c7c6ae282f9c84e2704481f82becaa
danfiscus <dafiscus@gmail.com>
2016-04-09 16:13:19 -0400

    Initial Code Commit
    I know it includes a bunch of `codelite` IDE junk, but oh well.

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/*================================*/
+/*
+ Simple program to solve all sides
+ and angles of a triangle. This
+ program is free for anyone to do
+ anything with (provided they comply
+ with the MIT license.
+*/
+/*================================*/
+
+#include <iostream>				//For input and output
+#include <cmath>					//For advanced math functions, like trigonometric functions
+
+using namespace std;
+
+/*GLOBAL VARIABLES*/
+const float PI = 3.14159265;		//Constant value for pi
+
+
+int main()
+{
+	float alpha,beta,gamma,a,b,c;	//alpha, beta, and gamma are angles a, b, and c respectively, and a,b, and c are sides across from the same-letter angles
+	
+}
